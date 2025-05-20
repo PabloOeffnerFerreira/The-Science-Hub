29ac6e9c9416b53c922344a433c729306242fa8f
Pablo <pofbremen@gmail.com>
2025-05-20 14:43:57 +0200

    Fixed Bug where the program wouldn't start because the coder imported wrong

--- tools/coder.py
+++ tools/coder.py
@@ -5,12 +5,12 @@ from PyQt6.QtWidgets import (
 )
 from PyQt6.QtGui import QFont, QColor, QAction, QTextCursor
 from PyQt6.QtCore import Qt
-from data_utils import register_window
+from tools.data_utils import register_window
 import io
 import contextlib
 import subprocess
 import json
-from data_utils import (
+from tools.data_utils import (
     results_dir, mineral_favs_path, element_favs_path, ptable_path,
     mineral_db_path, gallery_dir, gallery_meta_path, log_path, chain_log_path,
     exports_dir, settings_path, library_file, load_settings, load_element_data, ai_chatlogs_dir
